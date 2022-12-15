325ffa20e97dc3c251ce0940665e3608eb6c74db
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2022-12-15 22:37:41 +0300

    add header, dar light, input and styled input container

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Container } from "./Container";
 import { IoMoonOutline, IoMoon } from "react-icons/io5";
 
-const HeaderEl = styled.header``;
-const Wrapper = styled.div``;
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: (--colors-ui-base);
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 const Title = styled.a.attrs({
   href: "/",
-})``;
-const ModeSwitcher = styled.div``;
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`;
 
 export const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
-          <ModeSwitcher>
-            <IoMoon /> Тёмная тема
+          <Title>Где страна, дядь?</Title>
+          <ModeSwitcher onClick={changeTheme}>
+            {theme === "light" ? (
+              <IoMoonOutline size='14px' />
+            ) : (
+              <IoMoon size='14px' />
+            )}
+            <span style={{ marginLeft: "0.75rem" }}>
+              {" "}
+              {theme === "light" ? "Тёмная" : "Светлая"} тема{" "}
+            </span>
           </ModeSwitcher>
         </Wrapper>
       </Container>
