394f9a21e0f846b581928e7502ed31ea07e0bad5
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2022-12-17 13:34:17 +0300

    create axios and cards

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,8 +2,19 @@ import "./App.css";
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ALL_COUNTRIES } from "./constants";
 
 function App() {
+  const [countries, setCounries] = useState([]);
+
+  console.log(countries);
+
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCounries(data));
+  }, []);
+
   return (
     <>
       <Header />
--- app/src/index.js
+++ app/src/index.js
@@ -8,8 +8,8 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-    {/* <RouterProvider router={router} /> */}
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // {/* <RouterProvider router={router} /> */}
+  // </React.StrictMode>
 );
